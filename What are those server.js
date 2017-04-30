7b379b48cba0f39ba96e3aca482a631d90b9d6ab
Sean Brown <Sean Brown>
2017-04-30 14:40:18 -0400

    moved things

---
+++ What are those server.js
@@ -0,0 +1,30 @@
+/*
+this is our web server. It uses express.js to handle the get and 
+post request. The client will send a json object of an image and I will 
+take that image and send it to the cloudinary api.
+*/
+var http = require('http');
+var express = require('express');
+var net = require('net');
+var app = express();
+
+var client = new net.Socket();
+app.get('/',function(res,req){
+	req.send("<h1>Hello World!</h1>");
+});
+
+app.post('/',function(res,req){
+	console.log(req.body);
+	var content = JSON.stringify(req);
+	//loops through the json string
+	var pos = content.search(":");
+	var string = content.substring(pos+1,content.length);
+	console.log(string);
+});
+app.listen(3000,function(){
+	console.log("Server is working!");
+});
+
+client.connect(3000,'127.0.0.1',function(){
+	console.log("client has connected");
+});
