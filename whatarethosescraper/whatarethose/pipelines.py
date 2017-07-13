0eeffd5fdadb9d17dd46ff7d6763c18a92e3a726
Sean Brown <seanbrhn3@gmail.com>
2017-07-12 21:13:14 -0400

    re-added working scraper and database

--- whatarethosescraper/whatarethose/pipelines.py
+++ whatarethosescraper/whatarethose/pipelines.py
@@ -9,18 +9,3 @@
 class WhatarethosePipeline(object):
     def process_item(self, item, spider):
         return item
-
-class TotheDBPipline(object):
-   
-   def open_spider(self,spider):
-       self.file = sqlite3.connect('Whatarethose.db')
-       
-       
-   def proccess_item(self,item,spider):
-       conn = sqlite3.connect('Whatarethose.db')
-       c = conn.cursor() 
-       c.execute("CREATE images IF NOT EXISTS(id REAL,image BLOB, url TEXT)")
-       c.execute("INSERT INTO images VALUES('%s,%s')" % (item['files_urls'],item['files']))
-       conn.commit()
-       conn.close()
-       
