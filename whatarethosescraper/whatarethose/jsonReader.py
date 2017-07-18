0a06d27c42b45044738fc46ba969828f75272547
Sean Brown <seanbrhn3@gmail.com>
2017-07-18 13:14:51 -0400

    added stuff to justimgTest string table

--- whatarethosescraper/whatarethose/jsonReader.py
+++ whatarethosescraper/whatarethose/jsonReader.py
@@ -6,12 +6,12 @@ cur = connection.cursor()
 #open the json file to start reading it
 
 #execute the insert in to my reading the images and links keys and storing there values
-with open("justLinks.json") as json_file:
+with open("justImages.json") as json_file:
 	d = json.load(json_file)
 	for key in d:
 		for k,v in key.iteritems():
 			for link in v:
-				cur.execute("""INSERT INTO justlnk VALUES(?)""",(link,))
+				cur.execute("""INSERT INTO justimgTest VALUES(?)""",(link,))
 
 connection.commit()
 connection.close()
